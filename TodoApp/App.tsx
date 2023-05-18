1ba148abd9d5c149d73a84aff40d2b4c8e38862e
Lucia <wjddms4299@gmail.com>
2023-05-18 21:58:43 +0900

    day02

---
+++ TodoApp/App.tsx
@@ -0,0 +1,50 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React, {useState} from 'react';
+import {StyleSheet, KeyboardAvoidingView, Platform} from 'react-native';
+import DateHead from './components/DateHead';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import AddTodo from './components/AddTodo';
+import Empty from './components/Empty';
+import TodoList from './components/TodoList';
+
+function App() {
+  const today = new Date();
+
+  const [todos, setTodos] = useState([
+    {id: 1, text: '작업환경 설정', done: true},
+    {id: 2, text: '리액트 네이티브 기초 공부', done: false},
+    {id: 3, text: '투두리스트 만들어보기', done: false},
+  ]);
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView edges={['bottom']} style={styles.block}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+          style={styles.avoid}>
+          <DateHead date={today} />
+          {todos.length === 0 ? <Empty /> : <TodoList todos={todos} />}
+          <AddTodo />
+        </KeyboardAvoidingView>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  avoid: {
+    flex: 1,
+  },
+});
+
+export default App;
---
+++ TodoApp/components/TodoList.js
@@ -0,0 +1,29 @@
+/**
+ * @format
+ */
+
+import React from 'react';
+import {FlatList, View, Text, StyleSheet} from 'react-native';
+
+function TodoList({todos}) {
+    return (
+        <FlatList
+            style={styles.list}
+            data={todos}
+            renderItem={({item}) => (
+                <View>
+                    <Text>{item.text}</Text>
+                </View>
+            )}
+            KeyExtractor={item => item.id.toString()}
+        />
+    );
+}
+
+const styles = StyleSheet.create({
+    list: {
+        flex: 1,
+    },
+});
+
+export default TodoList;
