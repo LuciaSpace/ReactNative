c0f2c490fcfb448570830a463f1c8aabee8bb3a1
Lucia <wjddms4299@gmail.com>
2023-05-19 12:49:00 +0900

    day03

--- TodoApp/components/TodoList.js
+++ TodoApp/components/TodoList.js
@@ -4,16 +4,16 @@
 
 import React from 'react';
 import {FlatList, View, Text, StyleSheet} from 'react-native';
+import TodoItem from './TodoItem';
 
 function TodoList({todos}) {
     return (
         <FlatList
+            ItemSeparatorComponent={() => <View style={styles.separator} />}
             style={styles.list}
             data={todos}
             renderItem={({item}) => (
-                <View>
-                    <Text>{item.text}</Text>
-                </View>
+                <TodoItem id={item.id} text={item.text} done={item.done} />
             )}
             KeyExtractor={item => item.id.toString()}
         />
@@ -23,6 +23,10 @@ function TodoList({todos}) {
 const styles = StyleSheet.create({
     list: {
         flex: 1,
+    },
+    separator: {
+        backgroundColor: '#e0e0e0',
+        height: 1,
     },
 });
 
